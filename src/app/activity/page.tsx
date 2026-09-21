43bfcdaf9e190a76b2e572a245a43509628d81b0
phonarawd <4tencpkr@gmail.com>
2026-09-21 19:49:43 +0800

    feat: place mining activity inside app workspace

--- src/app/activity/page.tsx
+++ src/app/activity/page.tsx
@@ -1,5 +1,12 @@
+import { WorkspaceView } from "@/components/gpt/WorkspaceView";
 import { MiningActivity } from "@/components/mining/MiningActivity";
 
 export default function ActivityPage() {
-  return <MiningActivity />;
+  return (
+    <WorkspaceView>
+      <section className="app-view is-active" data-view="work">
+        <MiningActivity />
+      </section>
+    </WorkspaceView>
+  );
 }
